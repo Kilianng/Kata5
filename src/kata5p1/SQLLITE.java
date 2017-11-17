8695898a1f22eca4475e814a71f34378767cbe26
usuario <usuario@10.22.148.65>
2017-11-17 11:37:54 +0000

    Kata5P1 version 2

--- src/kata5p1/SQLLITE.java
+++ src/kata5p1/SQLLITE.java
@@ -5,6 +5,11 @@
 package kata5p1;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -20,7 +25,7 @@ public class SQLLITE {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
         // TODO code application logic here
         Class.forName("org.sqlite.JDBC");
         String urlConection = "jdbc:sqlite:C:/Users/usuario/Downloads/SQLiteDatabaseBrowserPortable/Data/katas.db";
@@ -34,5 +39,21 @@ public class SQLLITE {
             System.out.println(rs.getInt(1));
             System.out.println(rs.getString(2));
         }
+        
+        query = "CREATE TABLE IF NOT EXISTS MAIL ('Id' INTEGER PRIMARY KEY AUTOINCREMENT , 'Mail' TEXT NOT NULL)";
+        statement.execute(query);
+        
+        String fileString = "C:/Users/usuario/Downloads/SQLiteDatabaseBrowserPortable/Data/emailsfile.txt";
+        
+        BufferedReader reader = new BufferedReader(new FileReader(new File (fileString)));
+        String mail;
+        
+        while((mail=reader.readLine()) != null){
+            if(!mail.contains("@")) continue;
+            query = "INSERT INTO MAIL (mail) VALUES ('"+ mail +"')";
+            statement.executeUpdate(query);
+        }
+        
+        rs.close();
     }
 }
